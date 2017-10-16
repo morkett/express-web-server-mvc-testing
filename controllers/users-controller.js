5b9401301f0c9a6a8fc1492d3f36a7ac4570269e
morkett <dcorkett@gmail.com>
2017-10-16 09:53:55 +0100

    refactored for heroku

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,22 +1,52 @@
 var User = require('../models/user-model');
+var pg = require('pg');
 
-// Action: index
+// // Action: index
+// function indexUsers(req, res) {
+//   User.find({}, function (err, users) {
+//     if (err) {
+//       console.log('Could not get list of users:', err);
+//       // A little bit lazy, but not going to implement
+//       // anything more complex at this point in time:
+//       res.status(500).send('Could not get list of users');
+//       return;
+//     }
+//     res.render('users/index', {
+//       title: 'User list',
+//       users: users
+//     });
+//   });
+// }
+
 function indexUsers(req, res) {
-  User.find({}, function (err, users) {
+  pg.connect(process.env.DATABASE_URL+'?ssl=true', function(err, client, done) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  client.query('SELECT twitter_handle__c, sfid '+
+               'FROM salesforce.contact '+
+               'WHERE contact IS NOT NULL', function(err, result) {
     if (err) {
-      console.log('Could not get list of users:', err);
-      // A little bit lazy, but not going to implement
-      // anything more complex at this point in time:
-      res.status(500).send('Could not get list of users');
-      return;
-    }
+      console.error(err);
+    } else {
+      var contacts = {};
+      result.rows.forEach(function(row){
+        contacts[row.twitter_handle__c] = row;
+      });
+      console.log('contacts :', contacts);
+
     res.render('users/index', {
       title: 'User list',
-      users: users
+      users: contacts
     });
-  });
+  // });
 }
 
+
+//
+//
+//
 // Action: new
 function newUser(req, res) {
   res.render('users/new', {
